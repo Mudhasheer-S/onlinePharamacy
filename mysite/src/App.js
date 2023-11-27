01bbff39c3faafba9eea2e42a5de9eb5277ae40f
Mudhasheer S <mudhasheer0786@gmail.com>
2023-11-27 12:36:04 +0530

    add context provider

--- mysite/src/App.js
+++ mysite/src/App.js
@@ -11,25 +11,39 @@ import ContactUs from "./componentsForProject/Contact";
 import FAQ from "./componentsForProject/FAQ";
 import Products from "./componentsForProject/Products";
 import NeedHelp from "./componentsForProject/NeedHelp";
+import { createContext, useEffect, useState } from "react";
+import axios from "axios";
+
+export const MyProvider = createContext();
 
 function App() {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    axios.get('http://localhost:3001/userProfile/1').then((res) => {
+      setData(res.data)
+    })
+  }, [])
+
   return (
     <div>
-    <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/login" element={<LoginForm />} />
-        <Route path="/register" element={<SignUp />} />
-        <Route path="/Profile" element={<ManageProfile />} />
-        <Route path="/Orders" element={<OrderPage />} />
-        <Route path="/MyCart" element={<MyCart />} />
-        <Route path="/AboutUs" element={<AboutUs />} />
-        <Route path="/ContactUs" element={<ContactUs />} />
-        <Route path="/Products" element={<Products />} />
-        <Route path="/NeedHelp" element={<NeedHelp />} />
-        <Route path="/FAQ" element={<FAQ />} />
-        <Route path="/ProductDetail/:idValue" Component={ProductDetail} element={<ProductDetail />} />
-    </Routes>
-    </div> 
+      <MyProvider.Provider value={data}>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/login" element={<LoginForm />} />
+          <Route path="/register" element={<SignUp />} />
+          <Route path="/Profile" element={<ManageProfile />} />
+          <Route path="/Orders" element={<OrderPage />} />
+          <Route path="/MyCart" element={<MyCart />} />
+          <Route path="/AboutUs" element={<AboutUs />} />
+          <Route path="/ContactUs" element={<ContactUs />} />
+          <Route path="/Products" element={<Products />} />
+          <Route path="/NeedHelp" element={<NeedHelp />} />
+          <Route path="/FAQ" element={<FAQ />} />
+          <Route path="/ProductDetail/:idValue" Component={ProductDetail} element={<ProductDetail />} />
+        </Routes>
+      </MyProvider.Provider>
+    </div>
   );
 }
 
--- mysite/src/componentsForProject/ManageProfile.js
+++ mysite/src/componentsForProject/ManageProfile.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 import NavBar from './NavBar'
 import NavBar2 from './NavBar2'
 import ProfileNav from './profileNav'
@@ -8,6 +8,7 @@ import { Avatar, Button, Typography, Paper, Grid, Box, TextField } from '@mui/ma
 import "./ManageProfile.css"
 import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
+import { MyProvider } from '../App';
 function ManageProfile() {
     const [gender, setGender] = React.useState('');
     const [Fname, setFname] = React.useState('');
@@ -22,82 +23,85 @@ function ManageProfile() {
     const [userData, setData] = React.useState('');
     const navigator = useNavigate();
 
+    const context = useContext(MyProvider)
+
     const handleSubmit = async (event) => {
         event.preventDefault();
 
- 
+
+
         const responce = await axios.get(`http://localhost:3001/userData/${UserId}`)
-        
+
         const Submitted = {
-            name: Fname+ " " +Lname,
-            username:responce.data.username,
+            name: Fname + " " + Lname,
+            username: responce.data.username,
             dob: dob,
-            age:age,
-            password:responce.data.password,
-            phone:phone,
-            email:email,
-            gender:gender,
+            age: age,
+            password: responce.data.password,
+            phone: phone,
+            email: email,
+            gender: gender,
         };
         // console.log(Submitted);
 
-        await axios.put(`http://localhost:3001/userData/${UserId}`,Submitted)
-        .then(response => setData(response.data))
-        .catch(error => console.log(error))
+        await axios.put(`http://localhost:3001/userData/${UserId}`, Submitted)
+            .then(response => setData(response.data))
+            .catch(error => console.log(error))
 
     };
 
-    useEffect(() =>{
+    useEffect(() => {
         const fetchData = async () => {
-            const responce1 = await axios.get("http://localhost:3001/userProfile/1");
+            const responce1 = await axios.get("http://localhost:3001/userProfile/" + context.id);
 
-            if(responce1.data.profileValue == -1){
+            if (responce1.data.profileValue == -1) {
                 navigator("/login")
             }
             setUserId(responce1.data.profileValue);
             await axios.get(`http://localhost:3001/userData/${responce1.data.profileValue}`)
-            .then(responce => setData(responce.data))
-            .catch(error => console.log(error))
+                .then(responce => setData(responce.data))
+                .catch(error => console.log(error))
         }
         fetchData();
-        },[])
+    }, [])
 
     return (
         <div>
             <NavBar />
             <NavBar2 />
-            <Box sx={{ display: "flex", flexDirection: "row"}} spacing={1}>
+            <Box sx={{ display: "flex", flexDirection: "row" }} spacing={1}>
                 <Grid xs={4} sx={{ flex: "25%" }}>
                     <ProfileNav />
                 </Grid>
                 <Grid xs={8} sx={{ flex: "75%", padding: "35px" }}>
-                    <Box sx={{ height: "650px", width: "650px" ,boxShadow: "2px 1.2px 2px 1.2px grey" ,borderRadius:"29px"}}>
+                    <Box sx={{ height: "650px", width: "650px", boxShadow: "2px 1.2px 2px 1.2px grey", borderRadius: "29px" }}>
 
                         <Grid container direction="column" spacing={2} style={{ marginTop: "10px", alignItems: "center", justifyContent: "center" }}>
-                            <Avatar src="/broken-image.jpg" style={{ width: '100px', height: '100px' ,marginTop:"20px"}} />
+                            <Avatar src="/broken-image.jpg" style={{ width: '100px', height: '100px', marginTop: "20px" }} />
                             <Typography variant="h6"><b>{userData.name}</b></Typography>
                         </Grid>
                         <Grid style={{ paddingLeft: "20px" }}>
                             <h2>UserName:</h2>
-                            <h4 style={{  }}>{userData.username}</h4>
+                            <h4 style={{}}>{userData.username}</h4>
                             <hr />
                         </Grid>
                         <Grid style={{ paddingLeft: "20px" }}>
                             <h2>Email:</h2>
-                            <h4 style={{  }}>{userData.email}</h4>
+                            <h4 style={{}}>{userData.email}</h4>
                         </Grid>
                         <hr />
                         <Grid style={{ paddingLeft: "20px" }}>
                             <h2>Phone number:</h2>
-                            <h4 style={{ }}>{userData.phone}</h4>
+                            <h4 style={{}}>{userData.phone}</h4>
                         </Grid>
                         <hr />
                         <Grid style={{ paddingLeft: "20px" }}>
                             <h2>Age:</h2>
-                            <h4 style={{  }}>{userData.age}</h4>
+                            <h4 style={{}}>{userData.age}</h4>
                         </Grid>
 
                         <Popup trigger=
-                            {<Button variant='contained' color='success' size="large" sx={{ textAlign: 'center', paddingBottom: "6px" ,marginLeft:"250px"}}>EDIT</Button>} modal nested>
+                            {<Button variant='contained' color='success' size="large" sx={{ textAlign: 'center', paddingBottom: "6px", marginLeft: "250px" }}>EDIT</Button>} modal nested>
                             {
                                 close => (
                                     <div className='modal' style={{ height: "10px", display: 'flex', flexDirection: 'column', alignItems: 'center', backgroundColor: 'white', justifyContent: 'center', position: "relative" }}>
@@ -122,9 +126,9 @@ function ManageProfile() {
                                                                 id="firstName"
                                                                 label="Enter Your Phone Number"
                                                                 variant="standard"
-                                                                onChange={(e) => {setphone(e.target.value)}}
-                                                                // sx={{ '& .MuiInputBase-input': { color: 'red' } }}
-                                                                />
+                                                                onChange={(e) => { setphone(e.target.value) }}
+                                                            // sx={{ '& .MuiInputBase-input': { color: 'red' } }}
+                                                            />
                                                         </Grid>
                                                         <Grid item xs={12} sm={6}>
                                                             <TextField
@@ -133,7 +137,7 @@ function ManageProfile() {
                                                                 id="firstName"
                                                                 label="First Name"
                                                                 variant="standard"
-                                                                onChange={(e) => {setFname(e.target.value)}}
+                                                                onChange={(e) => { setFname(e.target.value) }}
                                                             // sx={{ '& .MuiInputBase-input': { color: 'red' } }}
                                                             />
                                                         </Grid>
@@ -144,7 +148,7 @@ function ManageProfile() {
                                                                 id="lastName"
                                                                 label="Last Name"
                                                                 variant="standard"
-                                                                onChange={(e) => {setLname(e.target.value)}}
+                                                                onChange={(e) => { setLname(e.target.value) }}
                                                             />
                                                         </Grid>
                                                         <Grid item xs={12}>
@@ -155,7 +159,7 @@ function ManageProfile() {
                                                                 label="Date of Birth"
                                                                 type="date"
                                                                 variant="standard"
-                                                                onChange={(e) => {setdob(e.target.value)}}
+                                                                onChange={(e) => { setdob(e.target.value) }}
                                                                 InputLabelProps={{
                                                                     shrink: true,
                                                                 }}
